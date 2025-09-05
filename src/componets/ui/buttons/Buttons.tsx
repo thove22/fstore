fca380a04c935cf17e93163cf10492ccaf2037a3
thove22 <pedrostovela@gmail.com>
2025-09-05 23:20:28 +0200

    feat:added responsiveness for the hero

--- src/componets/ui/buttons/Buttons.tsx
+++ src/componets/ui/buttons/Buttons.tsx
@@ -4,21 +4,21 @@ import { cn } from '@/lib/utils';
 
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center roundend none font-semibold uppercase tracking-wider text-sm transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 ',
+    'inline-flex items-center justify-center roundend none font-semibold uppercase tracking-wider text-sm',
     {
        variants: {
          variant:{
-            primary: 'bg-[#B88E2F] text-white hover:bg-opacity-90 focus:ring-[#B88E2F]'
+            primary: 'bg-[#B88E2F] text-white hover:bg-opacity-20 cursor-pointer'
                 ,
             secondary: 'bg-transparent border border-[#B88E2F] text-[#B88E2F] hover:bg-[#B88E2F] hover:text-white focus:ring-[#B88E2F]'
          },
          size:{
-            default:'py-3 px-10',
+            default:'py-3 px-10 lg:py-4 lg:px-12',
             lg:'py-4 px-12'
          },
          defaultVariants:{
             variant:'primary',
-            size:'default',
+            size:'default'
          }
        }
     }
